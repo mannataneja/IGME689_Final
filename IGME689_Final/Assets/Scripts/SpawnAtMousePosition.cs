3c7af1e75888e8f7e6357e03b4ac482f52211f6c
mannataneja <tt3092@g.rit.edu>
2025-12-02 22:19:47 -0500

    Spawning Point on Map

---
+++ IGME689_Final/Assets/Scripts/SpawnAtMousePosition.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class SpawnAtMousePosition : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject prefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("CLICK");
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Debug.Log("SPAWN");
+                GameObject point = Instantiate(prefab, hit.point, Quaternion.identity, this.transform.parent);
+            }
+        }
+    }
+}
