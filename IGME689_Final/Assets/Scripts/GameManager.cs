42caaf15560b1b9bfd6a6f397ae8d92910d284b5
mannataneja <tt3092@g.rit.edu>
2025-12-11 20:20:54 -0500

    Switching Between Scenes

--- IGME689_Final/Assets/Scripts/GameManager.cs
+++ IGME689_Final/Assets/Scripts/GameManager.cs
@@ -1,50 +1,77 @@
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-using System.Collections;
 using Esri.ArcGISMapsSDK.Components;
-using Esri.ArcGISMapsSDK.Utils.GeoCoord;
-using Esri.GameEngine.Geometry;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
     [SerializeField] public int minutes;
     [SerializeField] public int seconds;
+    public bool timeUp = false;
 
-    [SerializeField] TMP_Text minutesText;
-    [SerializeField] TMP_Text secondsText;
-
-    [SerializeField] ArcGISMapComponent map;
     [SerializeField] double longitude;
     [SerializeField] double latitude;
 
+    private void Awake()
+    {
+        CreateSingleton();
+    }
+
     private void Start()
     {
-        minutesText.text = minutes.ToString("00");
-        secondsText.text = seconds.ToString("00");
-
-        StartCoroutine(Timer());
-
-
-    }
-
-    public IEnumerator Timer()
-    {
-        while(minutes > 0 || seconds > 0)
+        if (SceneManager.GetActiveScene().buildIndex == 0)
         {
-            if(seconds > 0)
+            longitude = FindFirstObjectByType<FirstPersonController>().gameObject.GetComponent<ArcGISLocationComponent>().Position.X;
+            latitude = FindFirstObjectByType<FirstPersonController>().gameObject.GetComponent<ArcGISLocationComponent>().Position.Y;
+        }
+            
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.M) && !timeUp)
+        {
+            if(SceneManager.GetActiveScene().buildIndex == 0)
             {
-                seconds--;
+                minutes = FindFirstObjectByType<Timer>().minutes;
+                seconds = FindFirstObjectByType<Timer>().seconds;
+                LoadGlobeScene();
             }
-            else
+        }
+        if (Input.GetKey(KeyCode.S) && !timeUp)
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 1)
             {
-                seconds = 59;
-                minutes--;
+                minutes = FindFirstObjectByType<Timer>().minutes;
+                seconds = FindFirstObjectByType<Timer>().seconds;
+                LoadStreetView();
             }
-            minutesText.text = minutes.ToString("00");
-            secondsText.text = seconds.ToString("00");
-            yield return new WaitForSeconds(1);
+        }
+        if (timeUp && SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            minutes = FindFirstObjectByType<Timer>().minutes;
+            seconds = FindFirstObjectByType<Timer>().seconds;
+            LoadGlobeScene();
         }
     }
+    void CreateSingleton()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
 
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadGlobeScene()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        SceneManager.LoadScene(1);
+    }
+    public void LoadStreetView()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
---
+++ IGME689_Final/Assets/Scripts/Timer.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TMP_Text minutesText;
+    [SerializeField] TMP_Text secondsText;
+    [SerializeField] TMP_Text timeUpText;
+
+    public int minutes;
+    public int seconds;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        timeUpText.enabled = false;
+
+        minutes = FindFirstObjectByType<GameManager>().minutes;
+        seconds = FindFirstObjectByType<GameManager>().seconds;
+        minutesText.text = minutes.ToString("00");
+        secondsText.text = seconds.ToString("00");
+
+        StartCoroutine(StartTimer());
+    }
+
+    public IEnumerator StartTimer()
+    {
+        while (minutes > 0 || seconds > 0)
+        {
+            if (seconds > 0)
+            {
+                seconds--;
+            }
+            else
+            {
+                seconds = 59;
+                minutes--;
+            }
+            minutesText.text = minutes.ToString("00");
+            secondsText.text = seconds.ToString("00");
+            yield return new WaitForSeconds(1);
+        }
+        if (minutes == 0 && seconds == 0)
+        {
+            minutesText.text = minutes.ToString("00");
+            secondsText.text = seconds.ToString("00");
+            timeUpText.enabled = true;
+            yield return new WaitForSeconds(3);
+            FindFirstObjectByType<GameManager>().timeUp = true;
+        }
+    }
+}
